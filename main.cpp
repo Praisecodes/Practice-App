2db0bbeb0e0a1858b42d9cd058d3cfe1b418e52e
Praisecodes <cmancoder@gmail.com>
2022-08-18 11:55:36 +0100

    Function definition &&  LoginCall build

--- main.cpp
+++ main.cpp
@@ -9,6 +9,9 @@
 
 using json = nlohmann::json;
 
+//Functions For Calling API
+bool LoginCall(std::string username, std::string password, std::string endpoint);
+
 void Login(std::string API_endpoint);
 void SignUp(std::string API_endpoint);
 
@@ -81,4 +84,59 @@ int main() {
 			main();
 	}
 	return 0;
+}
+
+bool LoginCall(std::string username, std::string password, std::string endpoint) {
+	CURL* curl;
+	CURLcode response;
+	std::string* result;
+	std::string details = "{\"username\": \"" + username + "\", \"password\": \"" + password + "\"}";
+	json json_details = json::parse(details);
+
+	curl = curl_easy_init();
+	if (curl) {
+		struct curl_slist* headers = NULL;
+		headers = curl_slist_append(headers, "Content-Type: application/json");
+		headers = curl_slist_append(headers, "Cache-Control: must-revalidate, max-age=0, no-cache");
+		curl_easy_setopt(curl, CURLOPT_HTTPHEADER, headers);
+		curl_easy_setopt(curl, CURLOPT_URL, endpoint.c_str());
+		curl_easy_setopt(curl, CURLOPT_POST, 1);
+		curl_easy_setopt(curl, CURLOPT_COPYPOSTFIELDS, json_details.dump().c_str());
+		curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback);
+		curl_easy_setopt(curl, CURLOPT_WRITEDATA, &result);
+
+		response = curl_easy_perform(curl);
+		curl_easy_cleanup(curl);
+		if (response != CURLE_OK) {
+			return false;
+		}
+		else {
+			json json_result = json::parse(result);
+			std::vector<std::string> resultArr = json_result;
+			if (resultArr[0] == "Success") {
+				return true;
+			}
+			else {
+				return false;
+			}
+		}
+	}
+}
+
+void Login(std::string API_endpoint) {
+	std::string username, password;
+	std::cout << "Enter Your Username: ";
+	std::cin.ignore();
+	std::getline(std::cin, username);
+	std::cout << "Welcome " << username << ", Please Enter Your Password: ";
+	std::getline(std::cin, password);
+}
+
+void SignUp(std::string API_endpoint) {
+	std::string fullname, username, password;
+}
+
+static size_t WriteCallback(void* contents, size_t size, size_t nmemeb, void* userp) {
+	((std::string*)userp)->append((char*)contents, size * nmemeb);
+	return (size * nmemeb);
 }
